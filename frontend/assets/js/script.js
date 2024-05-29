63e6f445d8b0f1586c14949024fe30522131d6f1
Fr34k <cedric.steiger@gmail.com>
2024-05-29 22:05:21 +0200

    Add Domain-Accounts frontend/js

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -31,13 +31,74 @@ function updateDomainList() {
                 `;
             });
         } else {
-            domainList.innerHTML = '<div>Keine Domain-Accounts gefunden</div>';
+            domainList.innerHTML = '<div class="mb-6">Keine Domain-Accounts gefunden</div>';
         }
     })
     .catch(error => {
         console.error('Error fetching domain list:', error);
     });
 }
+
+// Event listener for form submission
+document.getElementById('domainAccountForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // disable submit button
+    const submitButton = document.getElementById('createDomainAccount');
+    submitButton.disabled = true;
+
+    const formData = new FormData(this);
+    formData.append('action', 'createDomainAccount'); // Add action to form data
+
+    // Send form data to backend
+    fetch('../backend/class-api.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Update modal with response
+        document.getElementById('result').classList.remove('text-green-500', 'text-red-500');
+        document.getElementById('result').classList.add(data.error ? 'text-red-500' : 'text-green-500');
+
+        if(data.error) {
+            document.getElementById('result').textContent = data.error;
+            submitButton.disabled = false;
+            return;
+        }
+
+        document.getElementById('result').textContent = data.success;
+        
+        updateDomainList();
+        submitButton.disabled = false;
+    })
+    .catch(error => {
+        console.log(error);
+        document.getElementById('result').classList.remove('text-green-500', 'text-red-500');
+        document.getElementById('result').classList.add('text-red-500');
+        document.getElementById('result').textContent = 'Ein Fehler ist aufgetreten';
+        submitButton.disabled = false;
+    });
+});
+
+// Event listener for modal open
+document.getElementById('createAccount').addEventListener('click', function() {
+    document.getElementById('modal').classList.remove('hidden');
+});
+
+// Close modal on ESC key press
+document.onkeydown = function(event) {
+    if (event.key === 'Escape') {
+        document.getElementById('modal').classList.add('hidden');
+    }    
+};
+
+// Close modal when clicking outside of it
+document.getElementById('modal').addEventListener('click', function(event) {
+    if (event.target.id === 'modal') {
+        document.getElementById('modal').classList.add('hidden');
+    }
+});
 
 // Initial list population on page load
 updateDomainList();
